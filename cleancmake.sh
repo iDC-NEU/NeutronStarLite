715b08339e8dba471a00f5c62530474a2961988b
Wangqge <1203508667@qq.com>
2021-09-29 17:10:41 +0800

    GCN example

---
+++ cleancmake.sh
@@ -0,0 +1,13 @@
+rm -r CMakeFiles
+rm CMakeCache.txt
+rm cmake_install.cmake
+rm -r core/CMakeFiles
+rm -r core/CMakeCache.txt
+rm -r core/cmake_install.cmake
+rm core/*.a
+rm core/*.so
+rm cuda/*.a
+rm -r cuda/CMakeFiles
+rm -r cuda/CMakeCache.txt
+rm -r cuda/cmake_install.cmake
+
