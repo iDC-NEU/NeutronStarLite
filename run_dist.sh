8fd6c969e9e677aa122cbff4bbb1cd52c584f178
Wangqge <1203508667@qq.com>
2021-10-20 13:21:38 +0800

    test

--- run_dist.sh
+++ run_dist.sh
@@ -3,7 +3,7 @@ make
 #mpiexec -np 1 ./myfistapp ./edge 875713 5
 #$1  training vertices      epoch      engine
 #mpiexec -np 2 ./myfistapp ./edge 875713 $1 $2
-mpiexec -np 2 ./myfistapp ./data/cora.2708.edge.self_write2208 2208 $1 $2
+mpiexec -np 2 ./nts ./data/cora.2708.edge.self_write2208 2208 $1 $2
 #mpiexec -np 2 ./myfistapp ./pubmed_data/pubmed.19717.edge_write15717 $1 $2 $3
 #mpiexec -np 2 ./myfistapp ./citeseer_data/citeseer.3327.edge_write2627 $1 $2 $3
 
