0f16b20b80ef989192533a86d0bf02540c77207e
Wangqge <1203508667@qq.com>
2021-12-22 12:59:33 +0800

    distributed tools

---
+++ make_and_copy.sh
@@ -0,0 +1,11 @@
+#i!/bin/bash
+
+cur_dir=$(cd "$(dirname "$0")"; pwd)
+cat hostfile| while read line; do
+ssh ${USER}@${WORKERS[$i]} "mkdir -p ${cur_dir}"
+#echo ${line}
+scp -r ./build  ${USER}@${line}:${cur_dir}/
+done
+
+
+
