34526ee4a21a73c2795b027fe4cfe1d6f7a93efe
Wangqge <1203508667@qq.com>
2022-06-09 22:07:32 +0800

    a sampling-based GCN toy

--- toolkits/main.cpp
+++ toolkits/main.cpp
@@ -1,5 +1,5 @@
 /*
-Copyright (c) 2014-2015 Xiaowei Zhu, Tsinghua University
+Copyright (c) 2021-2022 Qiange Wang, Northeastern University
 
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
@@ -12,17 +12,19 @@ Copyright (c) 2014-2015 Xiaowei Zhu, Tsinghua University
    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
    See the License for the specific language governing permissions and
    limitations under the License.
- */
+*/
 
+#include "GAT_CPU.hpp"
+#include "GCN_CPU_SAMPLE.hpp"
 #include "GCN_CPU.hpp"
 #include "GCN_CPU_EAGER.hpp"
+#include "GAT_CPU_DIST.hpp"
 #include "GIN_CPU.hpp"
-#include "GGNN_CPU.hpp"
-#include "GAT_CPU.hpp"
+#include "test_getdepneighbor_cpu.hpp"
 #if CUDA_ENABLE
+#include "test_getdepneighbor_gpu.hpp"
+#include "GAT_GPU_DIST.hpp"
 #include "COMMNET_GPU.hpp"
-//#include "GAT_GPU.hpp"
-//#include "GAT_GPU_SINGLE.hpp"
 #include "GCN.hpp"
 #include "GCN_EAGER.hpp"
 #include "GCN_EAGER_single.hpp"
@@ -62,6 +64,13 @@ int main(int argc, char **argv) {
     ntsGIN->init_graph();
     ntsGIN->init_nn();
     ntsGIN->run();
+  } else if (graph->config->algorithm == std::string("GCNSAMPLESINGLE")) {
+    graph->load_directed(graph->config->edge_file, graph->config->vertices);
+    graph->generate_backward_structure();
+        GCN_CPU_SAMPLE_impl *ntsGIN = new GCN_CPU_SAMPLE_impl(graph, iterations);
+    ntsGIN->init_graph();
+    ntsGIN->init_nn();
+    ntsGIN->run();
   } else if (graph->config->algorithm == std::string("GCNCPUEAGER")) {
     graph->load_directed(graph->config->edge_file, graph->config->vertices);
     graph->generate_backward_structure();
@@ -69,13 +78,13 @@ int main(int argc, char **argv) {
     ntsGCN->init_graph();
     ntsGCN->init_nn();
     ntsGCN->run();
-  } else if (graph->config->algorithm == std::string("GGNNCPU")) {
-    graph->load_directed(graph->config->edge_file, graph->config->vertices);
-    graph->generate_backward_structure();
-    GGNN_CPU_impl *ntsGGNN = new GGNN_CPU_impl(graph, iterations);
-    ntsGGNN->init_graph();
-    ntsGGNN->init_nn();
-    ntsGGNN->run();
+//  } else if (graph->config->algorithm == std::string("GGNNCPU")) {
+//    graph->load_directed(graph->config->edge_file, graph->config->vertices);
+//    graph->generate_backward_structure();
+//    GGNN_CPU_impl *ntsGGNN = new GGNN_CPU_impl(graph, iterations);
+//    ntsGGNN->init_graph();
+//    ntsGGNN->init_nn();
+//    ntsGGNN->run();
   } else if (graph->config->algorithm == std::string("GATCPU")) {
     graph->load_directed(graph->config->edge_file, graph->config->vertices);
     graph->generate_backward_structure();
@@ -83,11 +92,47 @@ int main(int argc, char **argv) {
     ntsGAT->init_graph();
     ntsGAT->init_nn();
     ntsGAT->run();
-  } 
+  }else if (graph->config->algorithm == std::string("GATCPUDIST")) {
+    graph->load_directed(graph->config->edge_file, graph->config->vertices);
+    graph->generate_backward_structure();
+    GAT_CPU_DIST_impl *ntsGAT = new GAT_CPU_DIST_impl(graph, iterations);
+    ntsGAT->init_graph();
+    ntsGAT->init_nn();
+    ntsGAT->run();
+//  }  else if (graph->config->algorithm == std::string("GGCNCPU")) {
+//    graph->load_directed(graph->config->edge_file, graph->config->vertices);
+//    graph->generate_backward_structure();
+//    GGCN_CPU_impl *ntsGAT = new GGCN_CPU_impl(graph, iterations);
+//    ntsGAT->init_graph();
+//    ntsGAT->init_nn();
+//    ntsGAT->run();
+  }
+  else if (graph->config->algorithm == std::string("test_getdep1")) {
+    graph->load_directed(graph->config->edge_file, graph->config->vertices);
+    graph->generate_backward_structure();
+    test_get_neighbor *ntsGAT = new test_get_neighbor(graph, iterations);
+    ntsGAT->init_graph();
+    ntsGAT->init_nn();
+    ntsGAT->run();
+  }
  
-
 #if CUDA_ENABLE
-  else if (graph->config->algorithm == std::string("COMMNETGPU")) {
+  else if (graph->config->algorithm == std::string("test_getdep")) {
+    graph->load_directed(graph->config->edge_file, graph->config->vertices);
+    graph->generate_backward_structure();
+    test_get_neighbor_gpu *ntsGAT = new test_get_neighbor_gpu(graph, iterations);
+    ntsGAT->init_graph();
+    ntsGAT->init_nn();
+    ntsGAT->run();
+  }
+    else if (graph->config->algorithm == std::string("GATGPUDIST")) {
+    graph->load_directed(graph->config->edge_file, graph->config->vertices);
+    graph->generate_backward_structure();
+    GAT_GPU_DIST_impl *ntsGAT = new GAT_GPU_DIST_impl(graph, iterations);
+    ntsGAT->init_graph();
+    ntsGAT->init_nn();
+    ntsGAT->run();
+    } else if (graph->config->algorithm == std::string("COMMNETGPU")) {
     graph->load_directed(graph->config->edge_file, graph->config->vertices);
     graph->generate_backward_structure();
     COMMNET_impl *ntsCOMM = new COMMNET_impl(graph, iterations);
@@ -125,21 +170,6 @@ int main(int argc, char **argv) {
     ntsGCN->init_graph();
     ntsGCN->init_nn();
     ntsGCN->run();
-    // GCN(graph, iterations);
-//  } else if (graph->config->algorithm == std::string("GAT")) {
-//    graph->load_directed(graph->config->edge_file, graph->config->vertices);
-//    graph->generate_backward_structure();
-//    GAT_GPU_impl *ntsGAT = new GAT_GPU_impl(graph, iterations);
-//    ntsGAT->init_graph();
-//    ntsGAT->init_nn();
-//    ntsGAT->run();
-//  } else if (graph->config->algorithm == std::string("GATSINGLE")) {
-//    graph->load_directed(graph->config->edge_file, graph->config->vertices);
-//    graph->generate_backward_structure();
-//    GAT_GPU_SINGLE_impl *ntsGAT = new GAT_GPU_SINGLE_impl(graph, iterations);
-//    ntsGAT->init_graph();
-//    ntsGAT->init_nn();
-//    ntsGAT->run();
   } else if (graph->config->algorithm == std::string("COMMNETGPU")) {
     graph->load_directed(graph->config->edge_file, graph->config->vertices);
     graph->generate_backward_structure();
@@ -154,7 +184,7 @@ int main(int argc, char **argv) {
     ntsGIN->init_graph();
     ntsGIN->init_nn();
     ntsGIN->run();
-  } 
+  }
 #endif
   exec_time += get_time();
   if (graph->partition_id == 0) {
